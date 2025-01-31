b57c7131c2c0dd392001f41246e5d8da4a6e6396
amanatulhay <amanatulhay.pribadi@gmail.com>
2025-01-31 23:35:35 +0800

    fix: increase size limit of image file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   
   app.enableCors();
   const config = new DocumentBuilder()
